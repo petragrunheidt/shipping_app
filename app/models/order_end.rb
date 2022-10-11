1465c6ee8920a164e597b158ff20db80e017257e
Petra <petra.grunheidt@gmail.com>
2022-10-11 10:42:54 -0300

    configures dates and deadlines for order_end

--- app/models/order_end.rb
+++ app/models/order_end.rb
@@ -1,3 +1,14 @@
 class OrderEnd < ApplicationRecord
   belongs_to :order
+  validate :ontime_order_with_reason
+
+  private
+
+  def ontime_order_with_reason
+    if self.time <= self.order.order_start.delivery_deadline and self.reason != ''
+      errors.add(:reason, ' deve ficar em branco pois não há atraso.')
+    elsif self.time > self.order.order_start.delivery_deadline and self.reason == ''
+      errors.add(:reason, ' não pode ficar em branco pois houve atraso.')
+    end
+  end
 end
--- spec/system/orders_spec/user_ends_order_spec.rb
+++ spec/system/orders_spec/user_ends_order_spec.rb
@@ -14,10 +14,8 @@ describe 'Usuário finaliza ordem de serviço' do
       click_on 'Finalizar Ordem de Serviço'
       # Assert
       expect(page).to have_field 'Data de Entrega'
-      #expect(page).to have_field 'Entrega dentro do prazo?'
       expect(page).to have_field 'Motivo do Atraso'
       expect(page).to have_button 'Finalizar Ordem de Serviço'
-
 
     end
     it 'com sucesso e sem atraso' do
@@ -26,7 +24,7 @@ describe 'Usuário finaliza ordem de serviço' do
       vehicle = FactoryBot.create(:vehicle, maximum_load: 80, transport_mode: delivery, status: :out_for_delivery)
       user = FactoryBot.create(:user)
       order = FactoryBot.create(:order, status: :on_route)
-      started_order = FactoryBot.create(:order_start, order: order, transport_mode: delivery, vehicle: vehicle)
+      started_order = FactoryBot.create(:order_start, order: order, transport_mode: delivery, vehicle: vehicle, delivery_deadline: Date.today)
 
 
       # Act
@@ -35,7 +33,6 @@ describe 'Usuário finaliza ordem de serviço' do
       click_on started_order.order.code
       click_on 'Finalizar Ordem de Serviço'
       fill_in 'Data de Entrega', with: Date.today
-      #select "Sim", :from => 'Entrega dentro do prazo?'
       click_on 'Finalizar Ordem de Serviço'
 
       # Assert
@@ -48,21 +45,41 @@ describe 'Usuário finaliza ordem de serviço' do
       expect(page).to have_content "Pedido entregue em #{I18n.localize(Date.today)}"
       expect(Vehicle.find(1).status).to eq "available"
     end
-    it 'com sucesso e com atraso' do
+    it 'e encontra erro ao tentar cadastrar motivo em finalização sem atraso' do
       # Arrange
       delivery = FactoryBot.create(:transport_mode, status: :active)
       vehicle = FactoryBot.create(:vehicle, maximum_load: 80, transport_mode: delivery, status: :out_for_delivery)
       user = FactoryBot.create(:user)
       order = FactoryBot.create(:order, status: :on_route)
-      started_order = FactoryBot.create(:order_start, order: order, transport_mode: delivery, vehicle: vehicle)
+      started_order = FactoryBot.create(:order_start, order: order, transport_mode: delivery, vehicle: vehicle, delivery_deadline: Date.today)
+
 
       # Act
       login_as(user)
       visit orders_path
       click_on started_order.order.code
       click_on 'Finalizar Ordem de Serviço'
-      fill_in 'Data de Entrega', with: Date.yesterday
-      #select "Não", :from => 'Entrega dentro do prazo?'
+      fill_in 'Data de Entrega', with: Date.today
+      fill_in 'Motivo do Atraso', with: 'Motivo'
+      click_on 'Finalizar Ordem de Serviço'
+
+      # Assert
+      expect(page).to have_content 'Motivo do Atraso deve ficar em branco pois não há atraso'
+    end
+    it 'com sucesso e com atraso' do
+      # Arrange
+      delivery = FactoryBot.create(:transport_mode, status: :active)
+      vehicle = FactoryBot.create(:vehicle, maximum_load: 80, transport_mode: delivery, status: :out_for_delivery)
+      user = FactoryBot.create(:user)
+      order = FactoryBot.create(:order, status: :on_route)
+      started_order = FactoryBot.create(:order_start, order: order, transport_mode: delivery, vehicle: vehicle, delivery_deadline: Date.yesterday)
+
+      # Act
+      login_as(user)
+      visit orders_path
+      click_on started_order.order.code
+      click_on 'Finalizar Ordem de Serviço'
+      fill_in 'Data de Entrega', with: Date.today
       fill_in 'Motivo do Atraso', with: 'Namorada da entregadora sonhou que as duas haviam terminado e ela precisou do dia de folga.'
       click_on 'Finalizar Ordem de Serviço'
 
@@ -73,69 +90,26 @@ describe 'Usuário finaliza ordem de serviço' do
       expect(page).to have_content vehicle.full_description
       expect(page).to have_content 'Valor do Frete:'
       expect(page).to have_content 'Data Prevista de Entrega:'
-      expect(page).to have_content "Pedido entregue em #{I18n.localize(Date.yesterday)}"
+      expect(page).to have_content "Pedido entregue em #{I18n.localize(Date.today)}"
       expect(page).to have_content 'Namorada da entregadora sonhou que as duas haviam terminado e ela precisou do dia de folga.'
     end
+    it 'e encontra erro ao tentar fazer cadastro com atraso sem motivo de atraso' do
+      # Arrange
+      delivery = FactoryBot.create(:transport_mode, status: :active)
+      vehicle = FactoryBot.create(:vehicle, maximum_load: 80, transport_mode: delivery, status: :out_for_delivery)
+      user = FactoryBot.create(:user)
+      order = FactoryBot.create(:order, status: :on_route)
+      started_order = FactoryBot.create(:order_start, order: order, transport_mode: delivery, vehicle: vehicle, delivery_deadline: Date.yesterday)
 
-  #   it 'e encontra erro quando não há veículo Disponível' do
-  #     # Arrange
-  #     user = FactoryBot.create(:user)
-  #     order = FactoryBot.create(:order, status: :pending)
-  #     delivery = FactoryBot.create(:transport_mode, status: :active)
-  #     vehicle = FactoryBot.create(:vehicle, maximum_load: 80, transport_mode: delivery, status: :maintenance)
+      # Act
+      login_as(user)
+      visit orders_path
+      click_on started_order.order.code
+      click_on 'Finalizar Ordem de Serviço'
+      fill_in 'Data de Entrega', with: Date.today
+      click_on 'Finalizar Ordem de Serviço'
 
-  #     # Act
-  #     login_as(user)
-  #     visit orders_path
-  #     click_on order.code
-  #     click_on 'Finalizar Ordem de Serviço'
-
-  #     # Assert
-  #     expect(page).to have_content 'No momento, não há Veículos disponíveis para essa Ordem de Serviço'
-  #   end
-
-  #   it 'e confirma o pedido com sucesso' do
-  #     # Arrange
-  #     user = FactoryBot.create(:user)
-  #     order = FactoryBot.create(:order, status: :pending, weight: 50)
-  #     delivery = FactoryBot.create(:transport_mode, min_weight: 1, max_weight: 100, status: :active)
-  #     vehicle = FactoryBot.create(:vehicle, maximum_load: 80, transport_mode: delivery, status: :available)
-
-  #     # Act
-  #     login_as(user)
-  #     visit orders_path
-  #     click_on order.code
-  #     click_on 'Finalizar Ordem de Serviço'
-  #     select delivery.with_weight_range, :from => 'Opções de Entrega'
-  #     click_on 'Finalizar Ordem de Serviço'
-  #     click_on 'Confirmar Ordem de Serviço'
-
-  #     # Assert
-  #     expect(page).to have_content "Ordem de Serviço confirmada."
-  #     expect(page).to have_content "Status: Em Processo de Entrega"
-  #     expect(Vehicle.find(1).status).to eq "out_for_delivery"
-
-  #   end
-
-  #   it 'e desconfirma o pedido com sucesso' do
-  #     # Arrange
-  #     user = FactoryBot.create(:user)
-  #     order = FactoryBot.create(:order, status: :pending, weight: 50)
-  #     delivery = FactoryBot.create(:transport_mode, min_weight: 1, max_weight: 100, status: :active)
-  #     vehicle = FactoryBot.create(:vehicle, maximum_load: 80, transport_mode: delivery, status: :available)
-
-  #     # Act
-  #     login_as(user)
-  #     visit orders_path
-  #     click_on order.code
-  #     click_on 'Finalizar Ordem de Serviço'
-  #     select delivery.with_weight_range, :from => 'Opções de Entrega'
-  #     click_on 'Finalizar Ordem de Serviço'
-  #     click_on 'Reconfigurar Ordem de Serviço'
-
-  #     # Assert
-  #     expect(page).to have_content "Inicie novamente a Ordem de Serviço."
-  #     expect(page).to have_content "Status: Pendente"
-
-  #   end
+      # Assert
+      expect(page).to have_content 'Motivo do Atraso não pode ficar em branco pois houve atraso.'
+    end
 end
